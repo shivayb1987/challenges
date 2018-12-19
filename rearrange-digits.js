88260eace7b859b2dbf481313d5d67223d9a4918
Shiva YB <shivayb1987@gmail.com>
2018-12-19 17:14:40 +0800

    Update rearrange-digits.js

--- rearrange-digits.js
+++ rearrange-digits.js
@@ -1,22 +1,22 @@
 /*
   Given a number of multiple digits,
   write an algorithm to squeeze 0s to the right and all non-zero digits to the left.
-
   constraint: It should do it in O(n)
-
   Example: Given 1020030450, output should be 1234500000
 */
 
 const assert = require('assert')
 function rearrange (num) {
   let result = 0
+  let multilpy = 1
   for (let i = 0; num > 0; i++) {
     let rem = num % 10
     if (rem > 0) {
-      result += (rem * Math.pow(10, i))
+      result += (rem * multilpy)
     } else {
       result *= 10
     }
+    multilpy *= 10
     num = Math.floor(num / 10)
   }
   return result
